e2cc86095714e84ee17f0d1fbd628e3e84bae1e5
JBemis54 <jacobbemis@gmail.com>
2023-03-31 21:53:16 -0400

    Pushing Project 2-Pokemon Classes

---
+++ cs121Folder/src/Project_2/Main.java
@@ -0,0 +1,13 @@
+package Project_2;
+
+public class Main {
+
+    public static void main(String[] args){
+
+        Pokemon_Selection newGame = new Pokemon_Selection();
+               newGame.assignPokemon();
+
+
+    }
+
+}
